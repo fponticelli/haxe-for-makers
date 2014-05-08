a7b1de0aeaaeaea437b2992e0bb68c37bbc93d0b
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-05-08 17:38:52 -0600

    added more contents

--- www/js/main.js
+++ www/js/main.js
@@ -1,19 +1,48 @@
 $(function() {
+	$('img[data-src]').each(function() {
+		$(this).attr('src', "images/blank.gif");
+	});
+
 	$('#presentation').jmpress({});
+
+	function enter() {
+		var self = this;
+		$('img[data-src]', self).each(function(_, iframe) {
+			$(iframe).attr("src", $(iframe).attr("data-src"));
+		});
+		setTimeout(function() {
+			$('iframe[data-src]', self).each(function(_, iframe) {
+				$(iframe).attr("src", $(iframe).attr("data-src"));
+			});
+		}, 2000);
+	}
+
+	$('.step')
+		.on('enterStep', enter)
+		.on('leaveStep', function() {
+			var self = this;
+			setTimeout(function() {
+				if(self === $('.active')[0])
+					return;
+				$('iframe[data-src],img[data-src]', self).each(function(_, iframe) {
+					$(iframe).attr("src", "images/blank.gif");
+				});
+			}, 6000);
+		});
 
 	// random logo effect
 	var $bg = $('body > div.bg'),
-		effects = ['blur'],
 		mindelay = 1000,
 		maxdelay = 8000,
 		delay = maxdelay;
 	function animate() {
-		var effect = effects[Math.floor(Math.random()*effects.length)];
-		$bg.toggleClass(effect);
+		$bg.toggleClass('blur');
 		setTimeout(animate, delay * Math.random());
 		if(Math.random() < .2)
 			delay = delay === maxdelay ? mindelay : maxdelay;
 	}
+
+	$('.step.active').each(enter);
 
 	animate();
 });
